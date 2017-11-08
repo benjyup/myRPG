a0a81d3b71c732a0e46b555712a9e6ecb49794e7
benjyup <31688754+benjyup@users.noreply.github.com>
2017-11-08 23:44:08 +0100

    :tada: Firsts mechanics of gameplay -> Movement & Interactions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,33 +9,51 @@ public class PlayerController : MonoBehaviour {
 	Camera cam;
 	PlayerMotor motor;
 	public LayerMask movementMask;
-	// Use this for initialization
+	public Interactable focus;
+
 	void Start () {
 		cam = Camera.main;
 		motor = GetComponent<PlayerMotor> ();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		if (Input.GetMouseButtonDown (0)) {
 			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
 
 			if (Physics.Raycast(ray, out hit, 100, movementMask)){
-
 				motor.MoveToPoint(hit.point);
-				// move our player
-
-				//Stop focusing any project
+				RemoveFocus();
 			}
 		}
-		else if (Input.GetMouseButtonDown (1)) {
+		if (Input.GetMouseButtonDown (1)) {
 			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
-			
+
 			if (Physics.Raycast(ray, out hit, 100, movementMask)){
-				// Check if we hit an object
+				Interactable interactable = hit.collider.GetComponent<Interactable>();
+
+				if (interactable != null){
+					SetFocus(interactable);
+				}
 			}
 		}
+	}
+
+	void SetFocus(Interactable newFocus){
+		if (newFocus != focus) {
+			if (focus != null)
+				focus.OnDefocused ();
+			focus = newFocus;
+			motor.FollowTarget (newFocus);
+		}
+		focus.OnFocused (transform);
+	}
+
+	void RemoveFocus(){
+		if (focus != null)
+			focus.OnDefocused ();
+		focus = null;
+		motor.StopFollowingTarget ();
 	}
 }
