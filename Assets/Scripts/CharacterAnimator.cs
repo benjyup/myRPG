33a3fe94ec44b48516c26a7e95cf9cbb788e598f
benjyup <31688754+benjyup@users.noreply.github.com>
2017-11-09 22:48:31 +0100

    :construction: Added/PlayerModel with animations

---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour {
+	Animator animator;
+	NavMeshAgent agent;
+	const float locomotionAnimationSmoothTime = .1f;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+		animator = GetComponentInChildren<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float speedPercent = agent.velocity.magnitude / agent.speed;
+		animator.SetFloat ("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+	}
+}
